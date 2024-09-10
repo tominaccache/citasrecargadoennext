0eea05ef062a8a7a4fe25164b569668104a865d3
Android Studio <devandroid@ort>
2024-09-10 13:52:46 -0300

    terminado

--- app/quienessomos/page.js
+++ app/quienessomos/page.js
@@ -10,7 +10,8 @@ export default function QuienesSomos() {
     <div className="container">
       <h2 className={styles.titulo}>Quiénes Somos</h2>
       <p className={styles.textoquienessomos}>Somos una empresa líder en la gestión de citas para mascotas, dedicada a facilitar el cuidado y bienestar de tus compañeros peludos. Entendemos que las mascotas son parte esencial de tu familia, y por eso nos enfocamos en ofrecer una plataforma intuitiva y eficiente para gestionar todas sus necesidades de salud y cuidado. Nuestro equipo, compuesto por profesionales con experiencia en el ámbito veterinario y tecnológico, trabaja incansablemente para que puedas programar citas con facilidad, desde chequeos regulares hasta tratamientos especializados. Nos enorgullece ser una solución confiable que te permite conectar con los mejores veterinarios, asegurando que tu mascota reciba la atención adecuada en el momento justo. En cada paso, nos guiamos por la pasión y el compromiso de mejorar la calidad de vida de los animales y facilitar la vida de sus dueños. Ya sea que necesites una consulta de rutina, una intervención quirúrgica o simplemente un control preventivo, estamos aquí para hacerlo más simple, rápido y eficiente. ¡Tu mascota es nuestra prioridad!</p>
-      <div>
+      {/*<div>
+        
         <p className={styles.textoquienessomos2}>Nuestro mejor veterinario:</p>
         <Image
           src="/imagenconejo.png"
@@ -19,7 +20,8 @@ export default function QuienesSomos() {
           alt="Imagen del mejor veterinario"
           style={{ width: '20%'}} 
         />
-      </div>        
+      </div>   
+      */}    
     </div>
   );
 }
